93b294627dc2c4bd47e74a22a32382c8c0b18cd0
danbuckland1 <buckd409@gmail.com>
2019-06-19 15:21:34 -0400

    trying to figure out carousel arrows. they keep disappearing.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var index = 0;
+var myTimer;
+var slideIndex = 1;
+var slideShow;
+
 slideShow();
 
 function slideShow() {
@@ -15,3 +19,28 @@ function slideShow() {
 
   setTimeout(slideShow, 4000);
 }
+
+function plusSlides(n) {
+  clearInterval(myTimer);
+  if (n < 0) {
+    slideShow((slideIndex -= 1));
+  } else {
+    slideShow((slideIndex += 1));
+  }
+  if (n === -1) {
+    myTimer = setInterval(function() {
+      plusSlides(n + 2);
+    }, 4000);
+  } else {
+    myTimer = setInterval(function() {
+      plusSlides(n + 1);
+    }, 4000);
+  }
+}
+
+window.addEventListener("load", function() {
+  slideShow(slideIndex);
+  myTimer = this.setInterval(function() {
+    plusSlides(1);
+  }, 4000);
+});
